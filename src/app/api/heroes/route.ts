7c6b5961fa51f12a909ae7cadec0a1f25e9b11cb
Lucas Lacerda <lribeirolacerda@gmail.com>
2024-10-10 16:10:33 -0300

    Termino

--- src/app/api/heroes/route.ts
+++ src/app/api/heroes/route.ts
@@ -2,6 +2,11 @@ import { NextResponse } from "next/server";
 
 import heroes from "./heroes.json";
 
+// export async function GET() {
+//   return NextResponse.json({ data: heroes });
+// }
 export async function GET() {
-  return NextResponse.json({ data: heroes });
+  return new Response(JSON.stringify(heroes), {
+    headers: { 'Content-Type': 'application/json' },
+  });
 }
