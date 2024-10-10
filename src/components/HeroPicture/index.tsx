b3b4062bedb041a81239ac2c83154c9473d233dd
Lucas Lacerda <lribeirolacerda@gmail.com>
2024-10-10 15:28:18 -0300

    Termino

--- src/components/HeroPicture/index.tsx
+++ src/components/HeroPicture/index.tsx
@@ -1,4 +1,7 @@
+// Bibliotecas externas
 import Image, { StaticImageData } from "next/image";
+
+// Importações locais
 import ImageSpiderMan616 from "@public/spiders/spider-man-616.png";
 import ImageSpiderMan1610 from "@public/spiders/spider-man-1610.png";
 import ImageSpiderWoman65 from "@public/spiders/mulher-aranha-65.png";
@@ -10,25 +13,25 @@ import ImageSpiderMan90214 from "@public/spiders/spider-man-90214.png";
 import { IHeroData } from "@/interfaces/heroes";
 
 const heroesImage: Record<string, StaticImageData> = {
-  "spider-man-616": ImageSpiderMan616,
-  "mulher-aranha-65": ImageSpiderWoman65,
-  "spider-man-1610": ImageSpiderMan1610,
-  "sp-dr-14512": ImageSpDr14512,
-  "spider-ham-8311": ImageSpiderHam8311,
-  "spider-man-90214": ImageSpiderMan90214,
-  "spider-man-928": ImageSpiderMan928,
+    "spider-man-616": ImageSpiderMan616,
+    "mulher-aranha-65": ImageSpiderWoman65,
+    "spider-man-1610": ImageSpiderMan1610,
+    "sp-dr-14512": ImageSpDr14512,
+    "spider-ham-8311": ImageSpiderHam8311,
+    "spider-man-90214": ImageSpiderMan90214,
+    "spider-man-928": ImageSpiderMan928,
 };
 
 interface IProps {
-  hero: IHeroData;
+    hero: IHeroData;
 }
 
 export default function HeroPicture({ hero }: IProps) {
-  return (
-    <Image
-      src={heroesImage[hero.id] || ImageSpiderMan616}
-      alt={`${hero.name} (Universo-${hero.universe})` || ""}
-      priority
-    />
-  );
+    return (
+        <Image
+            src={heroesImage[hero.id] || ImageSpiderMan616}
+            alt={`${hero.name} (Universo-${hero.universe})` || ""}
+            priority
+        />
+    );
 }
